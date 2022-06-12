e3a14a6d4ec6bd31c0e7c34105122e231a12a765
CullerWhale <jcullenharris@gmail.com>
2022-06-12 15:02:03 -0500

    controller progress

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,77 +1,98 @@
-const { Comment, Thought } = require('../models');
+const { User, Thought } = require('../models');
+const { get } = require('../models/Reaction');
 
-const commentController = {
-  // add comment to Thought
-  addComment({ params, body }, res) {
-    console.log(params);
-    Comment.create(body)
-      .then(({ _id }) => {
-        return Thought.findOneAndUpdate(
-          { _id: params.ThoughtId },
-          { $push: { comments: _id } },
-          { new: true }
-        );
-      })
-      .then(dbThoughtData => {
-        console.log(dbThoughtData);
-        if (!dbThoughtData) {
-          res.status(404).json({ message: 'No Thought found with this id!' });
-          return;
-        }
-        res.json(dbThoughtData);
-      })
-      .catch(err => res.json(err));
-  },
 
-  // add reply to comment
-  addReply({ params, body }, res) {
-    Comment.findOneAndUpdate(
-      { _id: params.commentId },
-      { $push: { replies: body } },
-      { new: true, runValidators: true }
-    )
-      .then(dbThoughtData => {
-        if (!dbThoughtData) {
-          res.status(404).json({ message: 'No Thought found with this id!' });
-          return;
-        }
-        res.json(dbThoughtData);
-      })
-      .catch(err => res.json(err));
-  },
+// addUser,
+//   getAllUsers,
+//   getOneUser,
+//   updateUser,
+//   deleteUser,
+//   removeFromBuddyList,
+//   addNewFriend,
 
-  // remove comment
-  removeComment({ params }, res) {
-    Comment.findOneAndDelete({ _id: params.commentId })
-      .then(deletedComment => {
-        if (!deletedComment) {
-          return res.status(404).json({ message: 'No comment with this id!' });
-        }
-        return Thought.findOneAndUpdate(
-          { _id: params.ThoughtId },
-          { $pull: { comments: params.commentId } },
-          { new: true }
-        );
-      })
-      .then(dbThoughtData => {
-        if (!dbThoughtData) {
-          res.status(404).json({ message: 'No Thought found with this id!' });
-          return;
-        }
-        res.json(dbThoughtData);
-      })
-      .catch(err => res.json(err));
-  },
-  // remove reply
-  removeReply({ params }, res) {
-    Comment.findOneAndUpdate(
-      { _id: params.commentId },
-      { $pull: { replies: { replyId: params.replyId } } },
-      { new: true }
-    )
-      .then(dbThoughtData => res.json(dbThoughtData))
-      .catch(err => res.json(err));
+const userController = {
+  //get all users
+  getAllUsers(req,res) {
+    User.find({}).select('-__v').then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
   }
-};
+}
 
-module.exports = commentController;
+// const commentController = {
+//   // add comment to Thought
+//   addComment({ params, body }, res) {
+//     console.log(params);
+//     Comment.create(body)
+//       .then(({ _id }) => {
+//         return Thought.findOneAndUpdate(
+//           { _id: params.ThoughtId },
+//           { $push: { comments: _id } },
+//           { new: true }
+//         );
+//       })
+//       .then(dbThoughtData => {
+//         console.log(dbThoughtData);
+//         if (!dbThoughtData) {
+//           res.status(404).json({ message: 'No Thought found with this id!' });
+//           return;
+//         }
+//         res.json(dbThoughtData);
+//       })
+//       .catch(err => res.json(err));
+//   },
+
+//   // add reply to comment
+//   addReply({ params, body }, res) {
+//     Comment.findOneAndUpdate(
+//       { _id: params.commentId },
+//       { $push: { replies: body } },
+//       { new: true, runValidators: true }
+//     )
+//       .then(dbThoughtData => {
+//         if (!dbThoughtData) {
+//           res.status(404).json({ message: 'No Thought found with this id!' });
+//           return;
+//         }
+//         res.json(dbThoughtData);
+//       })
+//       .catch(err => res.json(err));
+//   },
+
+//   // remove comment
+//   removeComment({ params }, res) {
+//     Comment.findOneAndDelete({ _id: params.commentId })
+//       .then(deletedComment => {
+//         if (!deletedComment) {
+//           return res.status(404).json({ message: 'No comment with this id!' });
+//         }
+//         return Thought.findOneAndUpdate(
+//           { _id: params.ThoughtId },
+//           { $pull: { comments: params.commentId } },
+//           { new: true }
+//         );
+//       })
+//       .then(dbThoughtData => {
+//         if (!dbThoughtData) {
+//           res.status(404).json({ message: 'No Thought found with this id!' });
+//           return;
+//         }
+//         res.json(dbThoughtData);
+//       })
+//       .catch(err => res.json(err));
+//   },
+//   // remove reply
+//   removeReply({ params }, res) {
+//     Comment.findOneAndUpdate(
+//       { _id: params.commentId },
+//       { $pull: { replies: { replyId: params.replyId } } },
+//       { new: true }
+//     )
+//       .then(dbThoughtData => res.json(dbThoughtData))
+//       .catch(err => res.json(err));
+//   }
+// };
+
+module.exports = userController;
